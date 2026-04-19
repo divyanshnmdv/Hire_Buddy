d7c1d4fc5e694f4c6654f3795e0e8853b0f08583
Dev <devnmdv@test.com>
2026-04-19 19:49:23 +0530

    ai model update

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -44,7 +44,7 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 `
 
     const response = await ai.models.generateContent({
-        model: "gemini-3-flash-preview",
+        model: "gemini-1.5-flash",
         contents: prompt,
         config: {
             responseMimeType: "application/json",

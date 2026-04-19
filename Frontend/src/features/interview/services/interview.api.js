261ca4bc5caff7303794c1156c780a4c1d06135d
Dev <devnmdv@test.com>
2026-04-19 16:22:57 +0530

    Url update build update

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 
-const apiUrl = import.meta.env.VITE_BACKEND_URL
+
 
 const api = axios.create({
-    baseURL: apiUrl,
+    baseURL: "https://hire-buddy.onrender.com",
     withCredentials: true,
 })
 
